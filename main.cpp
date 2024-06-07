d8b1aa341ab63a1a83618285220b5517fc702c88
nasodemo <nasodemo03@snu.ac.kr>
2024-06-07 07:49:19 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int cnt)
+{
+    if (cnt == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = 1; i <= N; i++)
+    {
+        if (!vec.empty() && vec.back() > i || !vec.empty() && vec.back() == i)
+        {
+            continue;
+        }
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
-int main() {
-   cin >> N >> M;
-   func(0);
+int main()
+{
+    cin >> N >> M;
+    func(0);
 }
